77f3bf9a9a69acb9052d00ecdc79e41111df452c
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-10-22 16:28:02 -0300

    adicionando style global

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Router } from 'react-router-dom';
 import history from './services/history';
 import Routes from './routes';
 import './config/ReactotronConfig';
+import GlobalStyle from './styles/global';
 // import { Container } from './styles';
 
 export default function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
