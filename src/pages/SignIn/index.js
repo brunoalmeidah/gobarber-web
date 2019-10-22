b21016de82dfdcf87d72e9a2f9e5c0b57b3b78a7
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-10-22 17:47:35 -0300

    configurando a lib unform da rocketseat

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '~/assets/logo.svg';
+import {Form, Input} from '@rocketseat/unform'
 // import { Container } from './styles';
 
 export default function SignIn() {
+  function handleSubmit(data){
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha secreta" />
+
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
