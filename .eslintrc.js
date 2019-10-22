e01410c6d6a6d0cc83e1416e93543825c6b09ec5
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-10-22 17:08:12 -0300

    adicionando Root Import

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,11 +36,11 @@ module.exports = {
     "react-hooks/exhaustive-deps": "warn",
     "react/jsx-props-no-spreading" : "off"
   },
- /* settings: {
+  settings: {
     "import/resolver": {
       "babel-plugin-root-import": {
         rootPathSuffix: "src"
       },
     },
-  },*/
+  },
 };
