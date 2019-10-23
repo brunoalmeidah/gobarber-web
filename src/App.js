31b3b09cfbcca1cfbdc0b0fb0f39bc100b73cfdb
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-10-23 07:54:42 -0300

    criando redux-persist

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import history from './services/history';
 import './config/ReactotronConfig';
 
 import Routes from './routes';
-import store from './store';
+import { store, persistor } from '~/store';
 import GlobalStyle from './styles/global';
 // import { Container } from './styles';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
