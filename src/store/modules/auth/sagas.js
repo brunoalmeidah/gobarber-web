259df7db7af2b0b2666019bb248b5525cb5bc535
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-10-23 08:12:40 -0300

    exibindo toast

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -13,13 +14,14 @@ export function* signIn({ payload }) {
 
     const { token, user } = response.data;
     if (!user.provider) {
-      console.tron.log('Usuário não é prestador');
+      toast.error('Usuário não é prestador');
       return;
     }
 
     yield put(signInSuccess(token, user));
     history.push('/dashboard');
   } catch (error) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
