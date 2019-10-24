d3e38d66360b4cb21a827ab436a6f0bf9daa8250
Bruno de Almeida Honorato <bruno.almeidah@gmail.com>
2019-10-23 21:24:08 -0300

    foto do perfil e dados do header

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+import AvatarInput from './AvatarInput';
 
 import { Container } from './styles';
 import { updateProfileRequest } from '~/store/modules/user/actions';
@@ -14,6 +15,7 @@ export default function Profile() {
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" placeholder="Seu endereço de e-mail" />
         <hr />
